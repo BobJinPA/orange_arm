7aac5093b93c58a575f23de73ca0aad1efd30803
Bob Jones <bobjinpa1@gmail.com>
2019-08-22 07:19:59 -0400

    troubleshooting

--- samples/controlled_servo_v2.py
+++ samples/controlled_servo_v2.py
@@ -5,10 +5,14 @@ import maestro
 
 servo = maestro.Controller("/dev/ttyAMA0")
 gamepad = InputDevice('/dev/input/event0')
+INCREMENT = 20
 
 servo.setAccel(0, 4)
 servo.setSpeed(0, 10)
 servo.setTarget(0, 6000)  # initial position
+print ("Current position: " + str(currentPosition))
+print ("get min: " + servo.getMin())
+print ("get max: " + servo.getMax())
 
 for event in gamepad.read_loop():
     currentPosition = servo.getPosition(0)
@@ -18,11 +22,12 @@ for event in gamepad.read_loop():
     #     break
     if event.type == 1:
         if event.code == 293 and event.value == 1:
-            print ("Top Right Button")
-            servo.setTarget(0, currentPosition + 100)
+            print ("Top Right Button: "+ currentPosition + INCREMENT)
+            servo.setTarget(0, currentPosition + INCREMENT)
         elif event.code == 292 and event.value == 1:
-            print ("Top Left Button")
-            servo.setTarget(0, currentPosition - 100)
+            print ("Top Left Button: " + currentPosition - INCREMENT)
+            servo.setTarget(0, currentPosition - INCREMENT)
+    print ("Current position: " + str(currentPosition))
 
 
 servo.close
